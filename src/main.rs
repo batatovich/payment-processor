ad42b0bb98a55258f4a3afabedcd3d7927bfb317
Pedro Llauradó <pllaurado1997@gmail.com>
2026-07-13 13:58:49 -0300

    use cache methods in services

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 mod constants;
 mod domain;
+mod storage;
 mod utils;
 
 use actix_web::{App, HttpResponse, HttpServer, Responder, Result, get, post, web};
-use rust_decimal::{Decimal, dec};
-use std::collections::HashSet;
-use std::path::Path;
 use std::sync::atomic::Ordering::Relaxed;
 
 use crate::domain::cache::Cache;
 use crate::domain::client::Client;
 use crate::domain::dto::{NewClientBody, NewCreditTransactionBody, NewDebitTransactionBody};
-
 use crate::utils::bootstrap;
 
 pub enum TransactionDirection {
@@ -19,7 +16,7 @@ pub enum TransactionDirection {
     Debit,
 }
 
-/// Endpoints 
+/// Endpoints
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -40,19 +37,44 @@ async fn new_client(
     cache: web::Data<Cache>,
 ) -> Result<impl Responder> {
     let body = req_body.into_inner();
+    let document_number = &body.document_number.clone();
+
+    // Check if document is already in use
+    if cache.is_document_in_use(document_number).await {
+        return Err(actix_web::error::ErrorConflict(
+            "A client with that document already exists.",
+        ));
+    }
+
+    // Set document in-flight to mark that this client is being created and has yet to be persisted to storage.
+    // The clients cache will only reflect the new client once its safely saved to storage.
+    cache
+        .set_document_in_flight(document_number)
+        .map_err(|e| actix_web::error::ErrorConflict(e))?;
 
     // New Client
     let new_client = Client::new(body);
 
-    let new_client_id = new_client.client_id.clone();
+    // Write the new client to storage
+    // If this returns an error, the client document will be removed from the in_flight cache
+    // and never added to the clients cache
+    if let Err(e) = crate::storage::save_client_to_storage(&new_client).await {
+        cache
+            .remove_document_in_flight(document_number)
+            .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
+        return Err(actix_web::error::ErrorInternalServerError(e));
+    }
 
-    // Insert the new client in the cache and save it to storage
-    cache.insert_client(new_client).await.map_err(|e| {
-        actix_web::error::ErrorInternalServerError(format!("Error inserting client: {e}"))
-    })?;
+    // Add the new client to the clients cache
+    cache.insert_client(&new_client).await;
+
+    // Remove new client document from in-flight cache
+    cache
+        .remove_document_in_flight(document_number)
+        .map_err(actix_web::error::ErrorInternalServerError)?;
 
     // Return the client id
-    Ok(HttpResponse::Ok().json(new_client_id.to_string()))
+    Ok(HttpResponse::Ok().json(new_client.client_id.to_string()))
 }
 
 #[post("/new_credit_transaction")]
@@ -66,7 +88,8 @@ async fn new_credit_transaction(
             req_body.credit_amount,
             TransactionDirection::Credit,
         )
-        .await;
+        .await
+        .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
 
     Ok(HttpResponse::Ok().json(new_balance))
 }
@@ -82,13 +105,41 @@ async fn new_debit_transaction(
             req_body.debit_amount,
             TransactionDirection::Debit,
         )
-        .await;
+        .await
+        .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
+
     Ok(HttpResponse::Ok().json(new_balance))
 }
 
 #[post("/store_balances")]
-async fn store_balances() -> impl Responder {
-    HttpResponse::Ok()
+async fn store_balances(cache: web::Data<Cache>) -> Result<impl Responder> {
+    // Lock the store guard.
+    // If we receive many requests to store_balances at the same time, we will effectively be making a queue here,
+    // waiting for earlier calls to finish writing to storage and udpating dirty clients cache.
+    // The lock is released when the function returns,
+    // and only there can another thread/task try to store balances again.
+    let _store_guard = cache.store_lock.lock().await;
+
+    // Snapshot which clients have balance changes to save
+    let dirty_clients = cache.get_dirty_clients()?;
+
+    // Early return if no news
+    if dirty_clients.is_empty() {
+        return Ok(HttpResponse::Ok());
+    }
+    let deltas_to_write = cache.collect_batch_deltas(&dirty_clients).await?;
+
+    let nonce = cache.nonce.load(Relaxed) + 1;
+
+    // Write to file.
+    // If anything here fails, the cache was never modified and we could retry the operation if needed.
+    crate::storage::save_balance_changes(nonce, &deltas_to_write).await?;
+
+    cache.apply_persisted_deltas(&deltas_to_write).await?;
+
+    cache.increment_nonce();
+
+    Ok(HttpResponse::Ok().into())
 }
 
 #[get("/client_balance")]
